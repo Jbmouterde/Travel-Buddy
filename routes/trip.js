04714301b79470d9d4089fd1f93dfb9eb0a8304c
Jb mout <jeanbaptiste.mouterde@kedgebs.com>
2018-04-23 16:41:00 +0200

    done

--- routes/trip.js
+++ routes/trip.js
@@ -18,14 +18,14 @@ tripRoutes.get("/home-user", (req,res,next)=> {
   //   res.redirect("/login")
   //   return 
   // }
-  User.find()
-  .then((userFromDb)=> {
-  res.locals.userList = userFromDb;
-  res.render("home-user/home-user")
-  })
-  .catch((err)=>{
-        next(err)
-  });
+  // User.find()
+  // .then((userFromDb)=> {
+  // res.locals.userList = userFromDb;
+  // res.render("home-user/home-user")
+  // })
+  // .catch((err)=>{
+  //       next(err)
+  // });
   //
   Trip.find( {owner : req.user._id})
   // add the details of the owner 
@@ -145,6 +145,36 @@ Trip.find( {owner : req.user._id})
 })
 ///////////
 // UPDATE THE TRIP 
+tripRoutes.get('/trips/:tripId/edit', (req ,res, next)=> {
+  Trip.findById(req.params.tripId)
+  .then((tripDetails)=>{
+  res.locals.tripId = req.params.tripId;
+  res.locals.trip = tripDetails;
+  res.render('home-user/update-trip');
+})
+  .catch((err)=>{
+next(err)
+  })
+})
+
+
+//update trip step 5 
+tripRoutes.post('/update-trip/:tripId', (req,res,next)=>{
+  // res.send(req.body);
+  const {destination} = req.body;
+ Trip.findByIdAndUpdate(
+    req.params.tripId,  // which document to update
+    {destination}, // what changes to make 
+    { runValidators: true}   // extra settings
+  )
+  .then(()=>{
+    res.redirect(`/home-user/home-user`)
+  })
+  .catch((err)=>{
+    next(err);
+  })
+})
+
 
 /////////
 // DELETE A TRIP 
