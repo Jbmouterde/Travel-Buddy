40f7fe773f325443b6baf9da61897f36b1cff3de
Jeremie Hercberg <jeremie.hercberg@gmail.com>
2018-04-24 11:10:33 +0200

    test link db & activitiese

--- routes/activity-router.js
+++ routes/activity-router.js
@@ -3,10 +3,52 @@ const passport = require("passport");
 const activityRoutes = express.Router();
 const User = require("../models/user-model");
 const Activity = require("../models/activity-model");
+const router = express.Router();
 
 //Routes
 
 // cf EXO LIBRARY
+
+router.get("/final-trip", (req, res, next) => {
+  console.log("titi");
+  Activity.find()
+    .populate("trip")
+    .then(activitiesfromDb => {
+      res.locals.activityList = activitiesfromDb;
+      res.render("process-activity");
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.post("process-activity", (req, res, next) => {
+  const {
+    typeOfActivity,
+    nameOfActivity,
+    activityDetail,
+    priceOfActivity
+  } = req.body;
+
+  Activity.create({
+    typeOfActivity,
+    nameOfActivity,
+    activityDetail,
+    priceOfActivity
+  })
+    .then(() => {
+      res.redirect("/final-trip");
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get("/final-trip/:activitiesId", (req, res, next) => {
+  console.log("titi");
+  res.locals.activitiesId = req.params.activitiesId;
+  res.render("final-trip");
+});
 
 // activityRoutes.post("/process-activity", (req, res, next) => {
 //   console.log("activity");
