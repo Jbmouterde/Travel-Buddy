c90c296886aa1f6440bf17e5e7c7ae93eaf49f50
Jb mout <jeanbaptiste.mouterde@kedgebs.com>
2018-04-27 08:52:06 +0200

    frifay

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -110,17 +110,17 @@ window.onclick = function(event) {
 };
 
 $("body > div.wrapper-final-message > div > div:nth-child(1) > button").click(function(){
-  $(".culture").slideToggle(1500)
+  $(".culture1").slideToggle(1500)
 });
 
 $("body > div.wrapper-final-message > div > div.restaurant > button").click(function(){
-  $(".restaurant").slideToggle(1500)
+  $(".restaurant1").slideToggle(1500)
 });
 
 $("body > div.wrapper-final-message > div > div.sport > button").click(function(){
-  $(".sport").slideToggle(1500)
+  $(".sport1").slideToggle(1500)
 });
 
 $("body > div.wrapper-final-message > div > div.hotel > button").click(function(){
-  $(".hotel").slideToggle(1500)
+  $(".hotel1").slideToggle(1500)
 });
